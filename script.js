efff4b2619c992d016e104b607db177f16e65125
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2020-11-15 20:42:28 +0100

    lastMinuteMode from 60 to 1 instead from 59 to 0

--- script.js
+++ script.js
@@ -163,6 +163,5 @@ function twoDigits(n) {
 // change the color for the text and for the background
 function changeColors(c, b) {
     document.body.style.color = c;
-    // document.body.style.background = b;
-    document.body.style.background = "brown";
+    document.body.style.background = b;
 }
