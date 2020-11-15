96d828423e7e6b053b5c120fe677a5194eed665e
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2020-11-15 19:49:21 +0100

    test

--- script.js
+++ script.js
@@ -112,6 +112,8 @@ function calculateCountdown() {
         }
         // calculate the total amount of seconds remaining till the end of the record
         secondsToEndToDoNow = secondsToEndToDos[i] - secondsNow;
+        // test
+        if (roundUpMins) { secondsToEndToDoNow = secondsToEndToDoNow + 60; }
     }
 
     // calculate countdown
@@ -158,5 +160,5 @@ function twoDigits(n) {
 function changeColors(c, b) {
     document.body.style.color = c;
     //document.body.style.background = b;
-    document.body.style.background = "green";
+    document.body.style.background = "salmon";
 }
